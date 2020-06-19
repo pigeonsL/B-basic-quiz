510a2d428f8596f1f36e3819d17eea3782d034c7
JingeLi <jinge.li@thoughtworks.com>
2020-06-19 21:10:06 +0800

    add user class

---
+++ src/main/java/tw/gtb/quiz/User.java
@@ -0,0 +1,11 @@
+package tw.gtb.quiz;
+
+public class User {
+
+    private long id;
+    private String name;
+    private long age;
+    private String avatar;
+    private String des;
+
+}
---
+++ src/main/java/tw/gtb/quiz/UserController.java
@@ -0,0 +1,4 @@
+package tw.gtb.quiz;
+
+public class UserController {
+}
