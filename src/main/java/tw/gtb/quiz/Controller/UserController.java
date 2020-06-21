73e937b7d3a2147edf3e13a65df8852b66fff728
JingeLi <jinge.li@thoughtworks.com>
2020-06-21 10:12:00 +0800

    add User controller

--- src/main/java/tw/gtb/quiz/Controller/UserController.java
+++ src/main/java/tw/gtb/quiz/Controller/UserController.java
@@ -5,12 +5,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import tw.gtb.quiz.User;
+import tw.gtb.quiz.UserService;
 
 @RestController
 public class UserController {
     @Autowired
+    UserService userService;
 
     @GetMapping("/users/{id}")
     public User getUser(@PathVariable("id")Integer id){
+        User user = userService.find(id);
+        return user;
     }
 }
